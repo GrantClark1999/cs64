a5376389b63682770200f6d8f703ab9d350b1006
Grant Clark <gclark@cstl-04.cs.ucsb.edu>
2019-11-01 11:48:15 -0700

    lab5

---
+++ lab5/DecodeCode.c
@@ -0,0 +1,17 @@
+#include "DecodeCode.h"
+
+mipsinstruction decode(int value)
+{
+	mipsinstruction instr;
+
+	instr.funct = value & 63;
+	instr.immediate = value & 65535;
+	instr.rd = (value >> 11) & 31;
+	instr.rt = (value >> 16) & 31;
+	instr.rs = (value >> 21) & 31;
+	instr.opcode = (value >> 26) & 63;
+
+	return instr;
+}
+
+
